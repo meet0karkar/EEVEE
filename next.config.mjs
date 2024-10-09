1000270df3962b619c8285ec44f8936f7841f768
karkar meet <75525924+meet0karkar@users.noreply.github.com>
2024-10-09 21:44:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost', 'eeveegoldsavings.com'],
-        unoptimized: true,
-    },
-     output: 'export'
+  images: {
+    domains: ['localhost', 'eeveegoldsavings.com'],
+    unoptimized: true,
+  },
+  output: 'export',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.126.98.206:3000/:path*' // Proxy to Backend
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
