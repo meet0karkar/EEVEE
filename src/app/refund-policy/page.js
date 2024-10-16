9633d1c3ab5394e050f44c3b0c9dfba642b5a548
karkar meet <75525924+meet0karkar@users.noreply.github.com>
2024-10-16 10:35:18 +0530

    Update page.js

--- src/app/refund-policy/page.js
+++ src/app/refund-policy/page.js
@@ -9,40 +9,40 @@ const Page = () => {
             <div className='lg:my-32  lg:mt-40 md:my-28  mt-24 mb-16 w-[90%] mx-auto text-[--policy-text] tracking-wide'>
                 <h1 className='font-bold text-[30px] text-white '>Refund Policy</h1>
                 <div>
-                    <p className='font-semibold mt-6 mb-4'>
-                        eeveegoldsavings.com is managed by EEVEE LIFESTYLE LLP
-                    </p>
-                    <p className='leading-relaxed'>
-                    If you  stop this plan that is totally ok but you can stop this plan between 16 month that we return only your deposit money&lsquo;s gold we can not offer extra month money when you are discontinue this plan between 16 month.
-                    </p>
-                    <br />
-                    <p className='leading-relaxed'>
-                        Whenever you are not able to continue this plan and if 2 month you skip than 3rd EMI	 is total cost of 3 month&lsquo;s amount + 5% charge pay till 1 to 10 date otherwise your plan is expired and you get your month&lsquo;s total deposit value&lsquo;s gold
-                    </p>
-                    <p className='leading-relaxed mt-4'>
-        This refund and cancellation policy outlines how you can cancel or seek a refund for a product/service that you have purchased through the Platform. Under this policy:
-    </p>
-    <p className='leading-relaxed mt-4'>
-        Cancellations will only be considered if the request is made within 7 days of placing the order. However, cancellation requests may not be entertained if the orders have been communicated to such sellers/merchant(s) listed on the Platform and they have initiated the process of shipping them, or if the product is out for delivery. In such an event, you may choose to reject the product at the doorstep.
-    </p>
-    <p className='leading-relaxed mt-4'>
-        LALIMA JEWELS LLP does not accept cancellation requests for perishable items like flowers, eatables, etc. However, a refund/replacement can be made if the user establishes that the quality of the product delivered is not good.
-    </p>
-    <p className='leading-relaxed mt-4'>
-        In case of receipt of damaged or defective items, please report to our customer service team. The request would be entertained once the seller/merchant listed on the Platform has checked and determined the same at its own end. This should be reported within 7 days of receipt of products.
-    </p>
-    <p className='leading-relaxed mt-4'>
-        If you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 7 days of receiving the product. The customer service team, after looking into your complaint, will take an appropriate decision.
-    </p>
-    <p className='leading-relaxed mt-4'>
-        For complaints regarding products that come with a warranty from the manufacturers, please refer the issue to them.
-    </p>
-    <p className='leading-relaxed mt-4'>
-        In case of any refunds approved by LALIMA JEWELS LLP, it will take 7 days for the refund to be processed to you.
-    </p>
-                    <br />
-                </div>
-            </div>
+          <p className="font-semibold mt-6 mb-4">
+            eeveegoldsavings.com is managed by EEVEE LIFESTYLE LLP
+          </p>
+          <p className="leading-relaxed">
+            If you stop this plan, that is totally okay; however, you can discontinue this plan only after 16 months, in which case we will return only the gold equivalent of your deposited money. We cannot offer extra benefits if you discontinue this plan before completing 16 months.
+          </p>
+          <br />
+          <p className="leading-relaxed">
+            Whenever you are unable to continue this plan and skip two months, the payment for the third month will be the total cost of three months’ amounts plus a 5% charge, which must be paid by the 1st to the 10th of the month; otherwise, your plan will expire, and you will receive gold equivalent to the total value of your monthly deposits.
+          </p>
+          <p className="leading-relaxed mt-4">
+            This refund and cancellation policy outlines how you can cancel or seek a refund for a product/service that you have purchased through the platform. Under this policy:
+          </p>
+          <p className="leading-relaxed mt-4">
+            Cancellations will only be considered if the request is made within 7 days of placing the order. However, cancellation requests may not be entertained if the orders have been communicated to such sellers/merchants listed on the platform and they have initiated the process of shipping them, or if the product is out for delivery. In such an event, you may choose to reject the product at the doorstep
+          </p>
+          <p className="leading-relaxed mt-4">
+            LALIMA JEWELS LLP does not accept cancellation requests for perishable items like flowers, eatables, etc. However, a refund or replacement can be made if the user establishes that the quality of the product delivered is not satisfactory.
+          </p>
+          <p className="leading-relaxed mt-4">
+            In case of receipt of damaged or defective items, please report to our customer service team. The request will be entertained once the seller/merchant listed on the platform has checked and confirmed the issue. This should be reported within 7 days of receipt of the products.
+          </p>
+          <p className="leading-relaxed mt-4">
+            If you feel that the product received is not as shown on the site or does not meet your expectations, you must inform our customer service within 7 days of receiving the product. The customer service team will review your complaint and take appropriate action.
+          </p>
+          <p className="leading-relaxed mt-4">
+            For complaints regarding products that come with a warranty from the manufacturers, please refer the issue directly to them.
+          </p>
+          <p className="leading-relaxed mt-4">
+            In case of any refunds approved by LALIMA JEWELS LLP, it will take 7 days for the refund to be processed.
+          </p>
+          <br />
+        </div>
+        </div>
             <Footer />
         </div>
     )
