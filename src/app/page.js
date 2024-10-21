3b2063946fea731abee6c9027a8e580d7c1293ff
karkar meet <75525924+meet0karkar@users.noreply.github.com>
2024-10-21 16:33:08 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -62,7 +62,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
         {/* second section */}
           <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row  justify-between items-center">
             <div className="lg:hidden w-full lg:w-1/2 flex justify-center items-center shrink-0">
